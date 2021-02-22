ee5f1805e979d51f68a29cc3bf0817bc5a6ad1b8
joaovictor3g <jvdias79797@gmail.com>
2021-02-22 13:15:08 -0300

    feat: experience bar transition with animation

--- moveit/src/components/ExperienceBar/index.tsx
+++ moveit/src/components/ExperienceBar/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.css';
 
 export function ExperienceBar() {
+    const [hover, setHover] = useState(false);
+
+    function onHover() {
+        setHover(true);
+    }
+
+    function onMouseLeave() {
+        setHover(false);
+    }
+
     return (
         <header className="experience-bar">
             <span>0 xp</span>
-            <div>
-                <div style={{ width: '50%'}} />
-                <span style={{ left: '50%' }} className="current-experience">300 xp</span>
+            <div 
+                onMouseOver={onHover} 
+                onMouseLeave={onMouseLeave}
+                style={hover ? { height: 10 } : {}}
+            >
+                <div 
+                    style={hover ? { width: '50%', height: 10 } : { width: '50%', height: 4 }} 
+                />
+                {hover &&
+                    <span style={{ left: '50%' }} className="current-experience">300 xp</span>
+                }
             </div>
             <span>600 xp</span>
         </header>
