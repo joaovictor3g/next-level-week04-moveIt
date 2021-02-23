3058a6ea326ca7ff8a30878a57e0f0a5fdecad74
joaovictor3g <jvdias79797@gmail.com>
2021-02-23 10:48:50 -0300

    feat: countDown Component

--- web-next/src/pages/index.tsx
+++ web-next/src/pages/index.tsx
@@ -1,18 +1,26 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | MoveIt</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div >
           <Profile />
           <CompletedChallenges />
+          <CountDown />
         </div>
 
         <div>
